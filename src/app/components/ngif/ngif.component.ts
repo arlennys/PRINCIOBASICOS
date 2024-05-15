f3fc29c0863bff0a65799fda5f70ededba279f74
arlennys <145227994+arlennys@users.noreply.github.com>
2024-05-15 10:38:44 -0400

    PRINCIPIOSBASICOS

---
+++ src/app/components/ngif/ngif.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngif',
+  templateUrl: './ngif.component.html',
+  styleUrl: './ngif.component.css'
+})
+export class NgifComponent {
+  showMessage=false;
+}
+ 
